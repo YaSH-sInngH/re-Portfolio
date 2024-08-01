824649e969162e84ef88486344b62a879d2f7b86
Yash <singhyash0026@gmail.com>
2024-08-01 15:48:39 +0530

    navbar-added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <div className='bg-b2 h-screen w-3/4 max-w-screen-lg mx-auto my-2 p-6 text-3xl text-center font-DM text-white font-bold rounded-xl'>
-        My Portfolio
+      <div className='bg-b2 h-screen w-3/4 max-w-screen-lg mx-auto my-2 p-6 text-center font-DM text-white font-bold rounded-xl'>
+        <Navbar/>
       </div>
     </>
   )
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-        <div>
-             
-        </div>
+      <div class="lg:sticky lg:top-20">
+        <nav class="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
+          <div class="flex flex-row space-x-0 pr-10">
+            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/">Home</a>
+            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/work">About</a>
+            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/blog">Projects</a>
+            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/blog">Contact</a>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
