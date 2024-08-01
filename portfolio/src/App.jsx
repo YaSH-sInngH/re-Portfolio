a85d62e71c0a77f658a5a8f5ad47c4250a5b8616
Yash <singhyash0026@gmail.com>
2024-08-01 10:21:06 +0530

    bg-color-template-added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      <div className='font-DM text-black font-bold m-2 p-6 text-3xl  text-center'>My Portfolio</div>
+      <div className='bg-b2 h-screen w-3/4 max-w-screen-lg mx-auto my-2 p-6 text-3xl text-center font-DM text-white font-bold rounded-xl'>
+        My Portfolio
+      </div>
     </>
   )
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -4,16 +4,30 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        LondrinaShadow: ['Londrina Shadow', 'sans-serif'],
-        LondrinaSolid: ['Londrina Solid', 'sans-serif'],
-        Coiny: ['Coiny', 'system-ui'],
-        Anton: ['Anton','sans-serif'],
-        DM: ['DM Serif Display','serif'],
+  theme: 
+  {
+    extend: 
+    {
+      colors: 
+      {
+        b1: '#1f2937',
+        b2: '#0f172a',
+        b3: '#1e293b',
+        b4: '#020617',
+        b5: '#111827',
+        b6: '#030712',
+        b7: '#171717',
+        b8: '#1a237e',
+
+        fontFamily: {
+          LondrinaShadow: ['Londrina Shadow', 'sans-serif'],
+          LondrinaSolid: ['Londrina Solid', 'sans-serif'],
+          Coiny: ['Coiny', 'system-ui'],
+          Anton: ['Anton','sans-serif'],
+          DM: ['DM Serif Display','serif'],
+        },
       },
-    },
+    }
   },
   plugins: [],
 }
