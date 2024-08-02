7ad441c9ca9fa1959abf9698464452197b3f1f96
Yash <singhyash0026@gmail.com>
2024-08-02 12:42:42 +0530

    home-page-added

---
+++ portfolio/src/components/Home/Home.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <section>
+            <p className='mx-3 mt-10 p-10 text-center text-5xl'>
+                Hi I am Yash 👋
+            </p>
+            <p className='text-center text-2xl font-semibold'>
+                I am a Developer, skilled in 
+                <a 
+                href="https://react.dev" 
+                target="_blank" 
+                className="inline-flex items-center rounded border border-neutral-200 bg-neutral-900 p-1 text-base leading-4 text-neutral-50 no-underline m-1"
+                >
+                    <img 
+                    src="/react.svg" 
+                    alt="React logo" 
+                    width="14" 
+                    height="14" 
+                    className="mr-1"
+                    />
+                        React
+                </a>
+                ,
+                <a 
+                href="https://nodejs.org/en" 
+                target="_blank" 
+                className="inline-flex items-center rounded border border-neutral-200 bg-neutral-900 p-1 text-base leading-4 text-neutral-50 no-underline m-1"
+                >
+                    <img 
+                    src="/nodejs-icon.svg" 
+                    alt="React logo" 
+                    width="14" 
+                    height="14" 
+                    className="mr-1"
+                    />
+                        Node
+                </a>
+                ,
+                <a 
+                href="https://expressjs.com/" 
+                target="_blank" 
+                className="inline-flex items-center rounded border border-neutral-200 bg-neutral-900 p-1 text-base leading-4 text-neutral-50 no-underline m-1"
+                >
+                    <img 
+                    src="/expressjs.svg" 
+                    alt="React logo" 
+                    width="14" 
+                    height="14" 
+                    className="mr-1 filter invert"
+                    />
+                        Express
+                </a>
+                and
+                <a 
+                href="https://www.mongodb.com/resources/products/fundamentals/basics" 
+                target="_blank" 
+                className="inline-flex items-center rounded border border-neutral-100 bg-neutral-900 p-1 text-base leading-4 text-neutral-50 no-underline m-1"
+                >
+                    <img 
+                    src="/mongodb-icon.svg" 
+                    alt="React logo" 
+                    width="14" 
+                    height="14" 
+                    className="mr-1"
+                    />
+                        Mongo
+                </a>
+                .
+                <br/>
+                I am also proficient in data structures and algorithms through 
+                <a 
+                href="https://www.geeksforgeeks.org/c-plus-plus/" 
+                target="_blank" 
+                className="inline-flex items-center rounded border border-neutral-100 bg-neutral-900 p-1 text-base leading-4 text-neutral-50 no-underline m-1"
+                >
+                    <img 
+                    src="/c++.svg" 
+                    alt="React logo" 
+                    width="14" 
+                    height="14" 
+                    className="mr-1"
+                    />
+                        C++
+                </a>
+                .
+            </p>
+        </section>
+    </>
+  )
+}
+
+export default Home
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-      <div class="lg:sticky lg:top-20">
-        <nav class="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
-          <div class="flex flex-row space-x-0 pr-10">
-            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/">Home</a>
-            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/work">About</a>
-            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/blog">Projects</a>
-            <a class="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/blog">Contact</a>
+      <div className="mt-8 mx-2">
+        <nav className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
+          <div className="flex flex-row space-x-0 pr-10">
+            <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/">Home</a>
+            <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/work">About</a>
+            <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/blog">Projects</a>
+            <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-2 px-4" href="/blog">Contact</a>
           </div>
         </nav>
       </div>
