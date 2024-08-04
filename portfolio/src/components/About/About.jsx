58d47fa23738fca1d9cd91189635d3cb2d947b66
Yash <singhyash0026@gmail.com>
2024-08-04 22:26:28 +0530

    about-page-succession

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -6,24 +6,110 @@ function About() {
         <div className='m-10'>
             <p className='my-10 p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor voluptatum sunt, accusantium officiis repellat quae odit accusamus ullam aspernatur laborum vitae, sapiente quia pariatur error aliquam tempora placeat. Cum aspernatur, ab ipsum obcaecati modi pariatur excepturi recusandae perferendis enim illo?</p>
             <p className='my-10 p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor voluptatum sunt, accusantium officiis repellat quae odit accusamus ullam aspernatur laborum vitae, sapiente quia pariatur error aliquam tempora placeat. Cum aspernatur, ab ipsum obcaecati modi pariatur excepturi recusandae perferendis enim illo?</p>
-            <h1>Skillset</h1>
-            <table className='w-full border-white border text-center'>
-                <tr className='bg-slate-950'>
-                    <td className='border-white border'>What I know!</td>
-                    <td className='border-white border'>What I do?</td>
-                    <td className='border-white border'>What I understand?</td>
-                </tr>
-                <tr className='bg-slate-950'>
-                    <td className='border-white border'>A</td>
-                    <td className='border-white border'>B</td>
-                    <td className='border-white border'>C</td>
-                </tr>
-                <tr className='bg-slate-950'>
-                    <td className='border-white border'>D</td>
-                    <td className='border-white border'>E</td>
-                    <td className='border-white border'>F</td>
-                </tr>
-            </table>
+            <div className='m-2 font-Coiny'>
+                <h1>Technologies, Tools and Frameworks that I use!</h1>
+                <div className="grid grid-cols-3 gap-4 border-white p-4">
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                            <img 
+                            src="/c++.svg" 
+                            alt="React logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
   )
