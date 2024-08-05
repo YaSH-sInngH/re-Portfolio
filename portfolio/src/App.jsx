9b347abfcda94a0a8984ee9bb3df98eb0a6c101e
Yash <singhyash0026@gmail.com>
2024-08-05 19:42:35 +0530

    update-abs

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,7 +9,7 @@ export class App extends Component {
   render() {
     return (
       <>
-        <div className="bg-b2 min-h-screen w-3/4 max-w-screen-lg mx-auto my-4 rounded-3xl p-6 font-DM text-white font-bold">
+        <div className="bg-b2 min-h-screen w-3/4 max-w-screen-lg mx-auto p-6 font-DM text-white font-bold">
         <div className="container mx-auto my-2 mt-4 rounded-xl">
           <Navbar />
           <hr className="mt-2 border-t-2 border-neutral-300" />
--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function About() {
   return (
     <>
-        <div className='m-10'>
-            <p className='my-10 p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor voluptatum sunt, accusantium officiis repellat quae odit accusamus ullam aspernatur laborum vitae, sapiente quia pariatur error aliquam tempora placeat. Cum aspernatur, ab ipsum obcaecati modi pariatur excepturi recusandae perferendis enim illo?</p>
-            <p className='my-10 p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor voluptatum sunt, accusantium officiis repellat quae odit accusamus ullam aspernatur laborum vitae, sapiente quia pariatur error aliquam tempora placeat. Cum aspernatur, ab ipsum obcaecati modi pariatur excepturi recusandae perferendis enim illo?</p>
+        <div className='m-12'>
+            <p className='my-10 p-2 font-thin'>I am a Developer, a tech enthusiast, who loves to spend time on coding and creating stuffs. I try to put visualized things, into a coding structure to make it a reality. Created few projects from the skills i gather in my coding, and developing journey to showcase how much productive and efficient i am in my coding. </p>
+            <p className='my-10 p-2 font-thin'>In my journey of coding and builting websites and projects, I used a hell lot of tech stack, but that's not the end, I intend on learning more and more tech-stack for future projects. Everyone one has a mantra or motto to look forward to, and mine is <i>"All we have is learning, we can learn from everywhere within time, so don't worry and keep moving forward."</i></p>
             <div className='m-2 font-Coiny'>
-                <h1>Technologies, Tools and Frameworks that I use!</h1>
+                <h1>Technologies, Tools and Frameworks that I use! & <i>"Click on the logo, to know where I learn all that from!</i></h1>
                 <div className="grid grid-cols-3 gap-4 border-white p-4">
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://www.youtube.com/playlist?list=PLu0W_9lII9aiXlHcLx-mDH1Qul38wD3aR"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/c.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -24,7 +24,7 @@ function About() {
                         <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
                             <img 
                             src="/c++.svg" 
-                            alt="React logo" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -32,10 +32,10 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://www.youtube.com/playlist?list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/java.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -43,10 +43,10 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://youtu.be/G3e-cpL7ofc?si=GWewNlPudvMrYmnJ"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/html1.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -54,10 +54,10 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://youtu.be/G3e-cpL7ofc?si=GWewNlPudvMrYmnJ"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/css2.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -65,10 +65,10 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://www.youtube.com/playlist?list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/javascript.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -76,10 +76,10 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://youtu.be/Oe421EPjeBE?si=ntsKsWRyHUZeSrx1"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/nodejs-icon.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -87,10 +87,10 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://www.youtube.com/playlist?list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/react.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
@@ -98,13 +98,145 @@ function About() {
                         </a>
                     </div>
                     <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
-                        <a href="https://www.geeksforgeeks.org/c-plus-plus/"  target="_blank">
+                        <a href="https://youtu.be/Oe421EPjeBE?si=ntsKsWRyHUZeSrx1"  target="_blank">
                             <img 
-                            src="/c++.svg" 
-                            alt="React logo" 
+                            src="/expressjs.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 invert cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://youtu.be/J6mDkcqU_ZE?si=Bfh7qAnGrr6-SvZJ"  target="_blank">
+                            <img 
+                            src="/mongodb-icon.svg" 
+                            alt="logo" 
                             width="44" 
                             height="44" 
                             className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.youtube.com/playlist?list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige"  target="_blank">
+                            <img 
+                            src="/redux.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://youtu.be/ezldKx-jPag?si=OdYnppnSOdNC2sTx"  target="_blank">
+                            <img 
+                            src="/figma.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://youtu.be/vpAJ0s5S2t0?si=8jM0SsEa_kKsYI9B"  target="_blank">
+                            <img 
+                            src="/bootstrap.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.youtube.com/playlist?list=PLu0W_9lII9ahwFDuExCpPFHAK829Wto2O" target="_blank">
+                            <img 
+                            src="/tailwindcss.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://pugjs.org/api/getting-started.html"  target="_blank">
+                            <img 
+                            src="/pugjs.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://code.visualstudio.com/docs"  target="_blank">
+                            <img 
+                            src="/vs-code.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://git-scm.com/doc"  target="_blank">
+                            <img 
+                            src="/git.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 invert cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.youtube.com/playlist?list=PLDzeHZWIZsTpukecmA2p5rhHM14bl2dHU"  target="_blank">
+                            <img 
+                            src="/mysql-official.svg" 
+                            alt="logo" 
+                            width="84" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://vitejs.dev/guide/"  target="_blank">
+                            <img 
+                            src="/vite.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://www.postman.com/postman/workspace/postman-public-workspace/documentation/12959542-c8142d51-e97c-46b6-bd77-52bb66712c9a"  target="_blank">
+                            <img 
+                            src="/getpostman-icon.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 cursor-pointer"
+                            />
+                        </a>
+                    </div>
+                    <div className="border border-white p-2 flex justify-center bg-neutral-900 ">
+                        <a href="https://youtu.be/gwWKnnCMQ5c?si=h4bPM2s_Lt8RmiI_"  target="_blank">
+                            <img 
+                            src="/github.svg" 
+                            alt="logo" 
+                            width="44" 
+                            height="44" 
+                            className="mr-1 invert cursor-pointer"
                             />
                         </a>
                     </div>
